efc3a0a8a844e7b7b627e361e0d59175374e0227
SyabilAsna <125952089+SyabilAsna@users.noreply.github.com>
2025-05-13 16:59:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showLove() {
+    const msg = document.getElementById("message");
+    msg.classList.remove("hidden");
+    msg.style.color = "#e91e63";
+    msg.style.fontWeight = "bold";
+}
